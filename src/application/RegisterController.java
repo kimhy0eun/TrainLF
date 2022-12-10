bd3eb8aec6190c4d01e15d240282ad593667ae3c
SUYOUNG-YOO <suyoung_99@naver.com>
2022-12-11 00:20:56 +0900

    승무원 등록화면 기능

--- src/application/RegisterController.java
+++ src/application/RegisterController.java
@@ -21,19 +21,19 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 
 public class RegisterController implements Initializable {
-	@FXML
-	private TableView<User> table;
-	
-	@FXML
+   @FXML
+   private TableView<User> table;
+   
+   @FXML
     private TableColumn<User, String> TrainColumn;
-	
-	@FXML
+   
+   @FXML
     private TableColumn<User, Integer> DateColumn;
-	
-	@FXML
+   
+   @FXML
     private TableColumn<User, String> LostColumn;
-	
-	@FXML
+   
+   @FXML
     private TextField TrainField;
 
     @FXML
@@ -43,40 +43,53 @@ public class RegisterController implements Initializable {
     private TextField LostField;
    
     @FXML
-    private Button registerbtn;
+    private Button registerbtn, deletebtn;
     @FXML private StackPane register;
     
    
     
     static String complete; //스태틱을 안붙이면 씬을 넘어갈때 값이 저장이 안됨.
-	String getdestination() {
-		return complete;
-	}
+   String getdestination() {
+      return complete;
+   }
     
     
     
     ObservableList<User> list = FXCollections.observableArrayList(
-    		new User(new SimpleStringProperty("무궁화호"),new SimpleIntegerProperty(221010),new SimpleStringProperty("아이폰")));
+          new User(new SimpleStringProperty("무궁화호"),new SimpleIntegerProperty(221010),new SimpleStringProperty("아이폰")));
     
     @Override
     public void initialize(URL location, ResourceBundle resource) {
-    	TrainColumn.setCellValueFactory(cellData-> cellData.getValue().trainProperty());
-    	DateColumn.setCellValueFactory(cellData-> cellData.getValue().dateProperty().asObject());
-    	LostColumn.setCellValueFactory(cellData-> cellData.getValue().lostProperty());
-    	table.setItems(list);
-    	
-    	registerbtn.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-    		@Override
-    		public void handle(MouseEvent event) {
-    			table.getItems().add(new User(new SimpleStringProperty(TrainField.getText()), 
-    					new SimpleIntegerProperty(Integer.parseInt(DateField.getText())),
-    							new SimpleStringProperty(LostField.getText())));
-    		}
-    	});
+       TrainColumn.setCellValueFactory(cellData-> cellData.getValue().trainProperty());
+       DateColumn.setCellValueFactory(cellData-> cellData.getValue().dateProperty().asObject());
+       LostColumn.setCellValueFactory(cellData-> cellData.getValue().lostProperty());
+       table.setItems(list);
+       
+       registerbtn.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+          @Override
+          public void handle(MouseEvent event) {
+             table.getItems().add(new User(new SimpleStringProperty(TrainField.getText()), 
+                   new SimpleIntegerProperty(Integer.parseInt(DateField.getText())),
+                         new SimpleStringProperty(LostField.getText())));
+             TrainField.clear();
+             DateField.clear();
+             LostField.clear();
+             
+          }
+       });
+       
+       
     }
     
+    @FXML
+    private void delete(ActionEvent event) {
+    	table.getItems().removeAll(table.getSelectionModel().getSelectedItem());
+    	
+    }
+    
+    
     public void exit(ActionEvent event) {
-		Platform.exit();
-	}
+      Platform.exit();
+   }
  
 }
